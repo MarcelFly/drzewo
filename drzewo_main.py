cfcdf846c968006d0474a4299e37258baa1b5dbe
MarcelFly <62428763+MarcelFly@users.noreply.github.com>
2020-03-20 14:57:54 +0100

    Update drzewo_main.py

--- drzewo_main.py
+++ drzewo_main.py
@@ -1,52 +1,73 @@
-class Wezel():
-    def __init__(self,dane=None):
-        self.dane=dane
-        self.prawy=None
-        self.lewy=None
-    def suma_poddrzewa(self,dane):
-        suma=self.dane
-        if self.prawy!=None:
-            suma+=self.prawy.suma_poddrzewa(self.prawy.dane)
-        if self.lewy!=None:
-            suma+=self.lewy.suma_poddrzewa(self.lewy.dane)
-        return suma
-    def ilosc_wezlow_poddrzewa(self,dane):
-        ilosc = 1
-        if self.prawy != None:
-            ilosc += self.prawy.ilosc_wezlow_poddrzewa(self.prawy.dane)
-        if self.lewy != None:
-            ilosc += self.lewy.ilosc_wezlow_poddrzewa(self.lewy.dane)
-        return ilosc
-    def mediana(self,dane):
-        lista=[int(self.dane)]
-        if self.prawy != None:
-            lista.extend(self.prawy.mediana(self.prawy.dane))
-        if self.lewy != None:
-            lista.extend(self.lewy.mediana(self.lewy.dane))
-        return lista
-wartosci=[int(5),int(3),int(7),int(2),int(5),int(1),int(0),int(2),int(8),int(5)]
-W=[i for i in range(10)]
-for z,wartosc in enumerate(wartosci):
-    W[z]=Wezel()
-    W[z].dane=wartosc
-W[0].prawy,W[0].lewy=W[2],W[1]
-W[1].prawy,W[1].lewy=W[4],W[3]
-W[2].prawy,W[2].lewy=W[6],W[5]
-W[6].prawy,W[6].lewy=W[8],W[7]
-W[8].lewy=W[9]
-print("Dla jakiego numeru wezla policzyc sume, srednia i mediane w poddrzewie? Wezly sa ponumerowane w zakresie od 0-9")
-x=input()
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def sum_tree(self, data):
+        sum = self.data
+        if self.right != None:
+            sum += self.right.sum_tree(self.right.data)
+        if self.left != None:
+            sum += self.left.sum_tree(self.left.data)
+        return sum
+
+    def sum_nodes_tree(self, data):
+        nodes = 1
+        if self.right != None:
+            nodes += self.right.sum_nodes_tree(self.right.data)
+        if self.left != None:
+            nodes += self.left.sum_nodes_tree(self.left.data)
+        return nodes
+
+    def list_to_median(self, data):
+        list_tree = [int(self.data)]
+        if self.right != None:
+            list_tree.extend(self.right.list_to_median(self.right.data))
+        if self.left != None:
+            list_tree.extend(self.left.list_to_median(self.left.data))
+        return list_tree
+
+
+values = [
+    int(5),
+    int(3),
+    int(7),
+    int(2),
+    int(5),
+    int(1),
+    int(0),
+    int(2),
+    int(8),
+    int(5),
+]
+W = [i for i in range(10)]
+for z, values in enumerate(values):
+    W[z] = Node()
+    W[z].data = values
+W[0].right, W[0].left = W[2], W[1]
+W[1].right, W[1].left = W[4], W[3]
+W[2].right, W[2].left = W[6], W[5]
+W[6].right, W[6].left = W[8], W[7]
+W[8].right = W[9]
+
+print(
+    "Dla jakiego numeru wezla policzyc sume, srednia i mediane w poddrzewie? Wezly sa ponumerowane w zakresie od 0-9"
+)
+x = input()
 print("suma wartosci poddrzewa wynosi; ")
-print(W[int(x)].suma_poddrzewa(W[int(x)].dane))
+print(W[int(x)].sum_tree(W[int(x)].data))
 print("srednia wartosc to: ")
-print(W[int(x)].suma_poddrzewa(W[int(x)].dane)/W[int(x)].ilosc_wezlow_poddrzewa(W[int(x)].dane))
+print(W[int(x)].sum_tree(W[int(x)].data) / W[int(x)].sum_nodes_tree(W[int(x)].data))
 print("mediana wynosi")
-lista_posortowana=sorted(W[int(x)].mediana(W[int(x)].dane))
-a=len(lista_posortowana)
-if a%2==1:
-    print(lista_posortowana[int(a/2)])
+list_sorted = sorted(W[int(x)].list_to_median(W[int(x)].data))
+a = len(list_sorted)
+if a % 2 == 1:
+    median = list_sorted[int(a / 2)]
 else:
-    print((lista_posortowana[int(a/2)-1]+lista_posortowana[int(a/2)])/2)
+    median = (list_sorted[int(a / 2) - 1] + list_sorted[int(a / 2)]) / 2
+print(median)
+
 
 
 
